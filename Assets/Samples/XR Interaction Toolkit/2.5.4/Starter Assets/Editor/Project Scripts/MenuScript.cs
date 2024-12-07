60d4a2176c822b6c8aa49d06786bdc0cb5a7ac54
Elric2005 <marij@macalester.edu>
2024-12-07 11:42:45 -0600

    traversal and rotation(analogue)

--- Assets/Samples/XR Interaction Toolkit/2.5.4/Starter Assets/Editor/Project Scripts/MenuScript.cs
+++ Assets/Samples/XR Interaction Toolkit/2.5.4/Starter Assets/Editor/Project Scripts/MenuScript.cs
@@ -8,6 +8,11 @@ public class MenuScript : MonoBehaviour
 {
     public Button host;
     public Button client;
+    public GameObject menuCanvas;
+    public Transform leftHand; 
+    public Transform rightHand; 
+    public Transform mainCamera; 
+    private bool isMenuActive = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +22,36 @@ public class MenuScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (facing_camera(leftHand))
+        {
+            Activate_menu(leftHand);
+        }
+        else if (facing_camera(rightHand))
+        {
+            Activate_menu(rightHand);
+        }
+        else
+        {
+            menuCanvas.SetActive(false);
+            isMenuActive = false;
+        }
         
+    }
+    
+    private bool facing_camera(Transform hand)
+    {
+        Vector3 palm_direction = hand == rightHand ? -hand.right : hand.right;
+        float dotProduct = Vector3.Dot(palm_direction, mainCamera.forward);
+        return dotProduct > 0.5f;
+    }
+    private void Activate_menu(Transform hand)
+    {
+        if (!isMenuActive)
+        {
+            menuCanvas.SetActive(true);
+            menuCanvas.transform.position = hand.position;
+            menuCanvas.transform.rotation = hand.rotation;
+            isMenuActive = true;
+        }
     }
 }
