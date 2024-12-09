dc15b43424cd4f4bb3a618f7253e58bc0e9fb2fa
KianDUBS <knaeimi@macalester.edu>
2024-12-09 13:28:56 -0600

    Moved Scripts to Script Folder
    Fixed issue with Netcode by moving scripts to seperate scripts folder (under assets).

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,79 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//Made through a youtube guide by Dapper Dino
 
-public class HandMenu : MonoBehaviour
+public class MenuScript : MonoBehaviour
 {
-    public GameObject menuGameObject;
-    Canvas canvas;
-    public GameObject mainCamera;
-    public GameObject fountain; // Reference to the fountain GameObject for teleportation
-    public Text riddleCounterText; // Reference to the Text UI element to display the riddle progress
-    private int riddlesSolved = 0; // Track the number of riddles solved
-    private int totalRiddles = 4; // Assuming there are 4 riddles per player
-    
-    public GameObject indicator; // Player's pointing indicator
-    // public RiddleManager riddleManager; // Reference to the RiddleManager to interact with riddles
-    
+    public Button host;
+    public Button client;
+    public GameObject menuCanvas;
+    public Transform leftHand; 
+    public Transform rightHand; 
+    public Transform mainCamera; 
+    private bool isMenuActive = false;
     // Start is called before the first frame update
     void Start()
     {
-        canvas = menuGameObject.GetComponent<Canvas>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Are the look direction and the palm pointing in opposite directions?
-        if (Vector3.Dot(-transform.right, mainCamera.transform.forward) < -0.7f)
+        if (facing_camera(leftHand))
         {
-            menuGameObject.transform.rotation = Quaternion.AngleAxis(90.0f, transform.up) * transform.rotation;
-            menuGameObject.transform.position = transform.position - 0.1f * transform.right + 0.05f * transform.up;
-            canvas.enabled = true;
+            Activate_menu(leftHand);
+        }
+        else if (facing_camera(rightHand))
+        {
+            Activate_menu(rightHand);
         }
         else
         {
-            canvas.enabled = false;
+            menuCanvas.SetActive(false);
+            isMenuActive = false;
         }
-
-        // Update the riddle counter display
-        riddleCounterText.text = $"{riddlesSolved}/{totalRiddles}";
+        
     }
-
-    // Function to teleport the player to the fountain
-    public void TeleportToFountain()
+    
+    private bool facing_camera(Transform hand)
     {
-        if (fountain != null)
+        Vector3 palm_direction = hand == rightHand ? -hand.right : hand.right;
+        float dotProduct = Vector3.Dot(palm_direction, mainCamera.forward);
+        return dotProduct > 0.5f;
+    }
+    private void Activate_menu(Transform hand)
+    {
+        if (!isMenuActive)
         {
-            transform.position = fountain.transform.position; // Teleport to fountain
-            Debug.Log("Teleported to the fountain!");
-        }
-        else
-        {
-            Debug.LogWarning("Fountain reference not set.");
+            menuCanvas.SetActive(true);
+            menuCanvas.transform.position = hand.position;
+            menuCanvas.transform.rotation = hand.rotation;
+            isMenuActive = true;
         }
     }
-
-    // Function to increment the riddle counter when the player solves a riddle
-    // public void SolveRiddle(GameObject selectedObject)
-    // {
-    //     if (riddleManager.CheckAnswer(selectedObject))  // Check if the object is correct
-    //     {
-    //         riddlesSolved++;
-    //         Debug.Log("Riddle solved! Progress: " + riddlesSolved + "/" + totalRiddles);
-    //         FountainController.Instance.AddWater();  // Update fountain water level
-    //     }
-    //     else
-    //     {
-    //         Debug.Log("Incorrect answer. Hint may be generated for another player.");
-    //     }
-    // }
-
-    // // Function to generate a hint for another player if the player fails a riddle
-    // public void GenerateHint()
-    // {
-    //     riddleManager.GenerateHint();  // Trigger hint generation in RiddleManager
-    // }
 }
-
-
