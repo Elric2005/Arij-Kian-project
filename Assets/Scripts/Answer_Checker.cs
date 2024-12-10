cbec30eaff64c88ed62331c4b633755aebda20e6
Elric2005 <marij@macalester.edu>
2024-12-09 19:08:19 -0600

    pointer completed

--- Assets/Scripts/Answer_Checker.cs
+++ Assets/Scripts/Answer_Checker.cs
@@ -19,17 +19,17 @@ public class Answer_Checker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((playerID == 1 && Input.GetMouseButtonDown(0)) || (playerID == 2 && Input.GetMouseButtonDown(1)))
-        {
-            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        // if ((playerID == 1 && Input.GetMouseButtonDown(0)) || (playerID == 2 && Input.GetMouseButtonDown(1)))
+        // {
+        //     Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        //     RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                GameObject playerPointedObject = hit.collider.gameObject;
-                riddleManager.CheckAnswer(playerPointedObject, playerID);
-            }
-        }
+        //     if (Physics.Raycast(ray, out hit))
+        //     {
+        //         GameObject playerPointedObject = hit.collider.gameObject;
+        //         riddleManager.CheckAnswer(playerPointedObject, playerID);
+        //     }
+        // }
 
     }
     
