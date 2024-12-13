f7f9cfb89bb0924c1b1e746c7f736b264eb549db
KianDUBS <knaeimi@macalester.edu>
2024-12-12 19:51:50 -0600

    Added README file for Bret

---
+++ Assets/Scripts/README.cs
@@ -0,0 +1,12 @@
+// Team Name: Built and Ran
+// Group Members: Arij and Kian
+// After pulling and then building/running the most recent build of the project,
+// you will be spawned in the first scene, where you choose to be host/client.
+// After that, you and your partner will be spawned in the second scene. In it,
+// your goal is to answer eight riddles (four for the host and four for the client),
+// the answers being eight game objects scattered throughout the game world.
+// Once all eight are completed, you should be transported to the third scene,
+// which is a lush and restored enviroment. The final riddle will be in relation to
+// the fountain you're spawned at, and to end the game both players must point at it.
+// From there, you're transitioned to the fourth and
+// final scene, in which you can choose to restart.
